40830f14c5e6db9dda9da5a4f3b355b535a421c3
Aditi <aditichaudhary048@gmail.com>
2025-08-01 00:54:59 +0530

    issue review label and pr close greeter added

---
+++ bot/issue_review_needed_labeler.py
@@ -0,0 +1,47 @@
+import asyncio
+import os
+import sys
+import traceback
+import aiohttp
+from aiohttp import web
+from gidgethub import aiohttp as gh_aiohttp
+from gidgethub import routing
+from gidgethub import sansio
+from gidgethub import apps
+
+router = routing.Router()
+
+# LABEL = 'review_needed' # label name
+LABEL1 = 'GSSoC22'
+LABEL2 = 'CX'
+
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+	label = event.data['issue']['labels_url']
+
+	installation_id = event.data["installation"]["id"]
+
+	issue_body = event.data["issue"]["body"]
+
+	installation_access_token = await apps.get_installation_access_token(
+		gh,
+		installation_id=installation_id,
+		app_id=os.environ.get("GH_APP_ID"),
+		private_key=os.environ.get("GH_PRIVATE_KEY")
+	)
+
+	# await gh.post(label, data=[LABEL],
+	#     oauth_token=installation_access_token["token"]
+	#              ) #event post for key label
+	if(issue_body.find("Community Exchange") == -1):
+		pass
+	else:
+		await gh.post(label, data=[LABEL2],
+		oauth_token=installation_access_token["token"]
+				 )
+
+	if(issue_body.find("GSSOC22") == -1):
+		pass
+	else:
+		await gh.post(label, data=[LABEL1],
+		oauth_token=installation_access_token["token"])
